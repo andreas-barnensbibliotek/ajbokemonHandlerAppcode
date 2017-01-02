86d78b21ebeb0bb00b7988f7252ad64c245c03e1
Andreas Josefsson <andreas@barnensbibliotek.se>
2017-01-02 08:59:49 +0100

    first commit

---
+++ Dal/bokemonDAL.vb
@@ -0,0 +1,213 @@
+﻿Imports Microsoft.VisualBasic
+Imports AJKontroller.webApiHelpers.bokemonHandler.Dal
+
+Namespace AJKontroller.webApiHelpers.bokemonHandler
+
+    Public Class bokemonDAL
+        Private _connectionString As String = ConfigurationSettings.AppSettings("SiteSqlServer") 'sätt en hänvisning till connectionstring i webconfig
+        Private _linqobj As New bokemonLinqDataContext(_connectionString)
+
+        Public Function getUserbokemon(ByVal userid As Integer) As bokemonInfo
+            Dim usermonsters As New bokemonInfo
+            Dim totalscore As Integer = 0
+            Dim usermonsterlist As New List(Of bokemonsterInfo)
+
+        Dim monsterlist = From m In _linqobj.AJ_getMonsterlistExt(userid)
+                          Select m
+
+            For Each monst In monsterlist
+                Dim mon As New bokemonsterInfo
+                mon.MonId = monst.monId
+                mon.BokemonSrc = monst.monstersrc
+                mon.Bokemoninfo = monst.monsterinfo
+                mon.Monsternamn = monst.monsternamn
+                mon.BokemonScore = monst.MonsterScore
+                mon.BokemonLevel = monst.monlevel
+                mon.BasePoints = monst.basepoint
+                mon.Bonus = monst.bonus
+                totalscore += monst.MonsterScore
+                usermonsterlist.Add(mon)
+
+            Next
+            usermonsters.BokemonList = usermonsterlist
+            usermonsters.Monvalue = totalscore
+            usermonsters.Userid = userid
+            usermonsters.TimeToNext = getmonstertimetonext(userid)
+
+            Return usermonsters
+
+        End Function
+
+        Public Function getAllbokemon(ByVal userid As Integer) As bokemonInfo
+            Dim usermonsters As New bokemonInfo
+            Dim usermonsterlist As New List(Of bokemonsterInfo)
+
+        Dim monsterlist = From m In _linqobj.tblmonsterLists
+                          Select m
+
+            For Each monst In monsterlist
+                Dim mon As New bokemonsterInfo
+                mon.MonId = monst.monId
+                mon.BokemonSrc = monst.monstersrc
+                mon.Bokemoninfo = monst.monsterinfo
+                mon.Monsternamn = monst.monsternamn
+                mon.BokemonScore = getbokemonscore(monst.basepoint, monst.bonus)
+                mon.BasePoints = monst.basepoint
+                mon.Bonus = monst.bonus
+                usermonsterlist.Add(mon)
+
+            Next
+            usermonsters.BokemonList = usermonsterlist
+            usermonsters.Userid = userid
+            usermonsters.TimeToNext = getmonstertimetonext(userid)
+
+            Return usermonsters
+
+        End Function
+
+        Public Function getAllbokdrakar(ByVal userid As Integer) As bokemonInfo
+            Dim bokdrakar As New bokemonInfo
+            Dim bokdraklistan As New List(Of bokemonsterInfo)
+
+        Dim monsterlist = From m In _linqobj.tblmonsterDrakelists
+                          Select m
+
+            For Each monst In monsterlist
+                Dim mon As New bokemonsterInfo
+                mon.MonId = monst.drakId
+                mon.BokemonSrc = monst.draksrc
+                mon.Bokemoninfo = monst.drakinfo
+                mon.Monsternamn = monst.draknamn
+                mon.BokemonScore = getbokemonscore(monst.drakbasepoint, monst.drakbonus)
+                mon.BasePoints = monst.drakbasepoint
+                mon.Bonus = monst.drakbonus
+                bokdraklistan.Add(mon)
+
+            Next
+            bokdrakar.BokemonList = bokdraklistan
+            bokdrakar.Userid = userid
+
+            Return bokdrakar
+
+        End Function
+
+        Private Function getbokemonscore(ByVal basepoint As Integer, ByVal bonus As Integer) As Integer
+            Dim ret As Integer
+            If bonus > 0 Then
+                ret = (bonus / 100) * basepoint
+                ret = basepoint + ret
+            Else
+                ret = basepoint
+            End If
+            Return ret
+
+        End Function
+        Public Function getmonstertimetonext(ByVal userid As Integer) As Integer
+            Dim ret As Integer = 0
+
+        Dim ntn = From c In _linqobj.tblmonsters
+                  Where c.userid = userid
+                  Select c
+
+            For Each usr In ntn
+                ret = usr.timetonext
+            Next
+
+            Return ret
+        End Function
+
+
+        Public Function updMonsterTimeToNext(ByVal userid As Integer, ByVal timetn As Integer) As Boolean
+            Dim ret As Boolean = False
+
+        Dim utm = (From t In _linqobj.tblmonsters
+                   Where t.userid = userid
+                   Select t).First()
+
+            Try
+                utm.timetonext = timetn
+                _linqobj.SubmitChanges()
+
+                ret = True
+            Catch ex As Exception
+                ret = False
+            End Try
+
+            Return ret
+        End Function
+
+
+        Public Function addMonsterToUser(ByVal userid As Integer, ByVal monsterid As Integer) As Boolean
+            Dim Inlagd As Boolean = False
+
+            Try
+                Dim utm As New tblmonsterToUser
+                utm.monid = monsterid
+                utm.userid = userid
+                utm.monlevel = 1
+                utm.MonsterScore = 1000
+
+                _linqobj.tblmonsterToUsers.InsertOnSubmit(utm)
+                _linqobj.SubmitChanges()
+
+                Inlagd = True
+
+            Catch ex As Exception
+                Inlagd = False
+            End Try
+
+            Return Inlagd
+        End Function
+
+
+        Public Function updMonsterToUser(ByVal userid As Integer, ByVal monsterid As Integer, ByVal monlev As Integer, ByVal monscore As Integer) As Boolean
+            Dim ret As Boolean = False
+
+        Dim utm = (From t In _linqobj.tblmonsterToUsers
+                   Where t.userid = userid And t.monid = monsterid
+                   Select t).First()
+            Try
+
+                utm.monlevel = monlev
+                utm.MonsterScore = monscore
+                _linqobj.SubmitChanges()
+
+                ret = True
+            Catch ex As Exception
+                ret = False
+            End Try
+
+            Return ret
+
+        End Function
+
+
+        
+
+#Region "Bokemon HELPER"
+
+        Public Function isBokemonInUserList(ByVal userid As Integer, ByVal monsterid As Integer) As Integer
+
+            Dim ret As Integer = 0
+
+        Dim utm = From t In _linqobj.tblmonsterToUsers
+                  Where t.userid = userid And t.monid = monsterid
+                  Select t
+            Try
+                If utm.Count > 0 Then
+                    For Each mon In utm
+                        ret = mon.MonsterScore
+                    Next
+                End If
+
+            Catch ex As Exception
+                ret = -99999
+            End Try
+
+            Return ret
+
+        End Function
+
+#End Region
+    End Class
+End Namespace
---
+++ bokemonController.vb
@@ -0,0 +1,138 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.DateTime
+Imports AJKontroller.webApiHelpers.bokemonHandler.Dal
+
+Namespace AJKontroller.webApiHelpers.bokemonHandler
+    Public Class bokemonController
+        Public Function getbaseUserAndMonsterList(ByVal userid As Integer) As bokemonInfo
+            Dim retobj As New bokemonInfo
+            Dim dalobj As New bokemonDAL
+            Try
+                retobj = dalobj.getUserbokemon(userid)
+                retobj.Status = "All user bokemon"
+            Catch ex As Exception
+                retobj.Status = "nått blev fel"
+            End Try
+
+            Return retobj
+
+        End Function
+
+        Public Function getbaseAllMonsterList(ByVal userid As Integer) As bokemonInfo
+            Dim retobj As New bokemonInfo
+            Dim dalobj As New bokemonDAL
+            Try
+                retobj = dalobj.getAllbokemon(userid)
+                retobj.Status = "All bokemons"
+            Catch ex As Exception
+                retobj.Status = "nått blev fel"
+            End Try
+
+            Return retobj
+
+        End Function
+
+
+
+        Public Function getbaseAllDrakarList(ByVal userid As Integer) As bokemonInfo
+            Dim retobj As New bokemonInfo
+            Dim dalobj As New bokemonDAL
+            Try
+                retobj = dalobj.getAllbokdrakar(userid)
+                retobj.Status = "Alla bokdrakar"
+            Catch ex As Exception
+                retobj.Status = "nått blev fel"
+            End Try
+
+            Return retobj
+
+        End Function
+
+
+        Public Function MonsterToUserList(ByVal userid As Integer, ByVal monsterid As Integer) As bokemonInfo
+            Dim retobj As New bokemonInfo
+            Dim dalobj As New bokemonDAL
+            Dim currentMonsterScore As Integer = 0
+
+
+            If monsterid > 0 Then
+
+                currentMonsterScore = dalobj.isBokemonInUserList(userid, monsterid)
+
+                If currentMonsterScore > 0 Then
+
+                    Dim newmonscore As Integer = calcMonsterScore("upd", currentMonsterScore)
+                    Dim newlevel As Integer = calcMonsterLevel(currentMonsterScore)
+
+                    Dim isupdated As Boolean = dalobj.updMonsterToUser(userid, monsterid, newlevel, newmonscore)
+                    retobj = dalobj.getUserbokemon(userid)
+
+                    If isupdated Then
+                        retobj.Status = "Monster updated to user: " & userid
+                    Else
+                        retobj.Status = "Nått blev fel när monster skulle uppdateras till user" & userid
+                    End If
+
+                Else
+                    Dim isadded As Boolean = dalobj.addMonsterToUser(userid, monsterid)
+                    retobj = dalobj.getUserbokemon(userid)
+                    If isadded Then
+                        retobj.Status = "Monster added to user: " & userid
+                    Else
+                        retobj.Status = "Nått blev fel när monster skulle läggas till user" & userid
+                    End If
+
+                End If
+            Else
+
+                retobj.Status = "Nått blev fel när monster skulle läggas till. Monsterid= " & monsterid
+            End If
+            'addTimetoNext(userid, timetn)
+
+            Return retobj
+
+        End Function
+
+
+        Private Function calcMonsterScore(ByVal typ As String, ByVal monScore As Integer, Optional ByVal lev As Integer = 1) As Integer
+            Dim retval As Integer
+
+            Select Case typ
+                Case "upd"
+                    retval = monScore + 200 'monScore * 1.2 'öka med 200poäng
+
+                Case "vinn"
+                    retval = (200 * lev) / 2 ' öka med 200 gånger bokmalslevel dela sedan med 2
+                    retval += monScore ' lägg till det tidigare monsterscorevärdet
+
+                Case "lose"
+                    If monScore >= 1000 Then
+                        If monScore < 1200 Then
+                            retval = 1000
+                        Else
+                            retval = -200
+                        End If
+                    Else
+                        retval = monScore
+                    End If
+
+            End Select
+
+            Return retval
+        End Function
+
+        Private Function calcMonsterLevel(ByVal monScore As Integer) As Integer
+            Dim retval As Integer
+            If monScore >= 1000 Then
+                Dim tmplv = monScore / 1000
+                retval = Math.Round(tmplv, 0)
+            Else
+                retval = 1
+            End If
+
+            Return retval
+        End Function
+
+
+    End Class
+End Namespace
